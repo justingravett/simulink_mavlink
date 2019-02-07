29e356ce4ded22a56fc0c4f59acc8957e7e59cbd
Justin Gravett <justin.gravett@esaero.com>
2019-02-06 16:29:27 -0800

    Setup Encoding/Decoding HIL Mavlink Messages

--- examples/example_create_sfunctions.m
+++ examples/example_create_sfunctions.m
@@ -9,21 +9,28 @@ this_dir = fileparts(mfilename('fullpath'));
 dir_sep = regexp(this_dir, filesep, 'split');
 cd(fullfile(dir_sep{1:end-1}));
 
+% TODO: Identify if Mavlink v1 or v2 to be used (v1 is always supported)
+
 % Select the MAVLink header files for messages we want to use
-filenames = {
-    'D:\simulink_mavlink\include\mavlink\v1.0\common\mavlink_msg_heartbeat.h';
-    'D:\simulink_mavlink\include\mavlink\v1.0\common\mavlink_msg_attitude.h';
-    'D:\simulink_mavlink\include\mavlink\v1.0\common\mavlink_msg_raw_imu.h';
+encode_filenames = {
+    'include\mavlink\v1.0\common\mavlink_msg_hil_state_quaternion.h';
+    %'include\mavlink\v1.0\common\mavlink_msg_hil_rc_inputs_raw.h';
+    %'include\mavlink\v1.0\common\mavlink_msg_hil_sensor.h';
+    %'include\mavlink\v1.0\common\mavlink_msg_hil_gps.h';
+    %'include\mavlink\v1.0\common\mavlink_msg_hil_optical_flow.h';
     };
 
+decode_filenames = {
+    'include\mavlink\v1.0\common\mavlink_msg_hil_actuator_controls.h';
+    };
 
 %% Create encoder s-functions
 % Calling create_sfun_encode will creates buses, header files, s-function
 % cpp files, and will finally compile them. This will create one encoder
 % s-function for each message
 
-for i = 1:length(filenames)
-    create_sfun_encode(filenames{i});
+for i = 1:length(encode_filenames)
+    create_sfun_encode(encode_filenames{i});
 end
 
 
@@ -32,4 +39,4 @@ end
 % cpp file, and will finally compile it. This will create one decoder
 % s-function in total
 
-create_sfun_decode(filenames);
+create_sfun_decode(decode_filenames);
